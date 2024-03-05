f5c2a61527c874da058ba79a7ea5ff90e570dfe5
Master Pi <minh.vln140501@gmail.com>
2024-03-05 08:24:19 +0000

    init

--- tests/localdymension/init.sh
+++ tests/localdymension/init.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 set -xeu
-rm -rf v1
-rm -rf v2
-rm -rf v3
-rm -rf v4
 
 MNE1="amused rural desk trick safe whip first menu worth swap enhance punch spin figure elevator abandon camera idea peace nurse coyote adjust modify produce"
 MNE2="relax output math high normal adapt include audit spend leader perfect husband nominee service federal neutral occur spider luxury mesh emerge head cabbage abuse"
 MNE3="predict alter night ugly venue speed labor exhibit embrace combine acquire ivory canal solar salad mirror invest life chapter decorate manage despair load pear"
 MNE4="rule jealous step victory tiny taste nose list field hawk various copy interest grass maximum puppy airport tree between popular deny cram cover under"
 
-mkdir v1 v2 v3 v4
-dymd init v1 --chain-id local_3300-1 --home v1 2> output1
-dymd init v2 --chain-id local_3300-1 --home v2 2> output2
-dymd init v3 --chain-id local_3300-1 --home v3 2> output3
-dymd init v4 --chain-id local_3300-1 --home v4 2> output4
+dymd init v1 --chain-id local_3300-1 --home v1 --overwrite 2> output1
+dymd init v2 --chain-id local_3300-1 --home v2 --overwrite 2> output2
+dymd init v3 --chain-id local_3300-1 --home v3 --overwrite 2> output3
+dymd init v4 --chain-id local_3300-1 --home v4 --overwrite 2> output4
 
 node1=$(cat output1 | jq -r '.node_id')
 node2=$(cat output2 | jq -r '.node_id')
 node3=$(cat output3 | jq -r '.node_id')
 node4=$(cat output4 | jq -r '.node_id')
+
+rm output*
 
 echo $MNE1 | dymd keys add v1 --recover --keyring-backend test --home v1
 echo $MNE2 | dymd keys add v2 --recover --keyring-backend test --home v2
